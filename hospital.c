0e1e247de004131e8fedeb6f3967b0fca7740e0b
aatnimsarani-al <aatnimsarani@gmail.com>
2026-09-18 19:30:37 +0530

    Modify average times for specialties
    Updated average consultation times for specialties.

--- hospital.c
+++ hospital.c
@@ -30,7 +30,7 @@ const char* SPECIALTY_NAMES[NUM_SPECIALTIES] = {
     "General Practice (OPD)", "Paediatrics", "Cardiology", "Neurology"
 };
 const float BASE_FEES[NUM_SPECIALTIES] = {1500.00, 2500.00, 4500.00, 5500.00};
-const int AVG_TIMES[NUM_SPECIALTIES] = {15, 20, 30, 30};
+const int AVG_TIMES[NUM_SPECIALTIES] = {10, 25, 30, 30};
 const int DAILY_CAPS[NUM_SPECIALTIES] = {30, 20, 12, 10};
 
 const char* WARD_NAMES[NUM_WARDS] = {
